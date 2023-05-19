cc3d2c72917e62ac96fc40dd709205b1b88bf575
MarceloSolusys <marcelo@solusys.com.br>
2023-05-18 22:45:22 -0300

    Initial commit

---
+++ lib/model/animal.dart
@@ -0,0 +1,15 @@
+class Animal {
+  int id;
+  String nome;
+  String avatar;
+  bool visible;
+  bool encontrado;
+
+  Animal({
+    this.id=0,
+    this.nome='',
+    this.avatar='',
+    this.visible=false,
+    this.encontrado=false
+  });
+}
---
+++ lib/screens/login.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const  Text('Login Agro APP'),),
+
+    );
+  }
+}
